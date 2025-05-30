1bc2b2023fbb89be9c667d289d39f5f802da5260
BigBabyofTel <augustus.tb@outlook.com>
2025-05-29 21:29:17 -0400

    Search component completed

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image"
-import { Button } from "@/components/ui/button";
+import { Button, TextField } from "@mui/material"
+import {InputAdornment} from "@mui/material"
+import SearchIcon from "@mui/icons-material/Search"
+
 
 // search icon is together with the input 
 export default function SearchBox() {
@@ -7,10 +10,22 @@ export default function SearchBox() {
      const search = "/icon-search.svg"
   return (
     <section className="w-11/12 p-2 border rounded-lg shadow-xl flex justify-around h-fit mx-auto">
-    <Image src={search} alt="search" width={20} height={20} className="flex-shrink-0"/>
-    <input type="text" placeholder="Search GitHub username..." className="">
-    </input>
-    <Button className="bg-blue-500 text-white">Search</Button>
+      <TextField id="input-with-icon-textfield"
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          },
+        }}
+       placeholder="Search GitHub profiles..."
+        variant="standard"
+        
+        />
+    <Button variant="contained">Search</Button>
+    
   </section>
   )
 }
